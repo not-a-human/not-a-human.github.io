618914bc45a1492d3fcb2c96cdae052e99ab6904
aviesinar <avie900@gmail.com>
2025-03-20 13:09:02 +0800

    Second commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Important for exporting as static files
+  images: {
+    unoptimized: true, // If you are using the Next.js image optimization feature, disable it for GitHub Pages
+  },
+  assetPrefix: isProd ? "/" : "", // Add a prefix in production
 };
 
 export default nextConfig;
