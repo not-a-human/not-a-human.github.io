613a71041aba2e7ec7b1de2f6e467ee215a35182
not-a-human <avie900@gmail.com>
2025-03-23 11:29:36 +0800

    update

--- app/ui/about.tsx
+++ app/ui/about.tsx
@@ -4,14 +4,16 @@ export function About() {
     return (
         <div>
         <h1 className='text-center' id='about'>About</h1>
-        <div className='flex'>
-            <div>
+        <div className={`${styles.aboutContainer}`}>
+            <div className={`${styles.aboutme}`}>
                <p>
-                    I am a software engineer based in Jakarta, Indonesia. I have a passion for web development and design. I am proficient in JavaScript, TypeScript, React, and NextJS. I am currently looking for a full-time position as a software engineer.
+                    I am a Software Engineer based in Malaysia. I have a passion for web development.
                 </p> 
+                <p>
+                I am open to job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then do not hesitate to contact me.
+                </p>
             </div>
-            
-            
+            <div className={`${styles.skills}`}></div>
         </div>
         
         </div>
