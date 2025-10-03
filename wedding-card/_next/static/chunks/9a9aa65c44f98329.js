b5ba3242df1dc2b7a0d144796f14a3af3fa9ebc3
not-a-human <not-a-human@users.noreply.github.com>
2025-10-03 18:33:30 +0000

    Deploy wedding card: 709167e87a4f5a168a0ff58b4900fc38650a8db0 not-a-human/wedding-card@709167e87a4f5a168a0ff58b4900fc38650a8db0

---
+++ wedding-card/_next/static/chunks/9a9aa65c44f98329.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/a242160b391ef46f.js",
+  "static/chunks/turbopack-3029eb542d982d4f.js"
+])
---
+++ wedding-card/_next/static/chunks/f30e6d2071faf4a1.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/a242160b391ef46f.js",
+  "static/chunks/turbopack-f4df6d22d5a9eddf.js"
+])
